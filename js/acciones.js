54d29160b7656193273821ac184d3792abde8ae3
alumno165ecb16 <andymendez.am39@gmail.com>
2014-12-09 12:51:10 -0600

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,7 @@
 //JavaScript
 $(document).ready(function(e){	
 $('#principal').height($('#page1').height());
-     document.addEventListener("deviceready",function());
+     document.addEventListener("deviceready",function(){
 	 
 	 audio=window.plugins.LowLatencyAudio;
 	 
